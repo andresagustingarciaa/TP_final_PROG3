c312cb694b8821c576e1904fcbf22058d450eda4
ibarranicolas <ibarranicolasj@gmail.com>
2026-05-08 09:01:55 -0300

    Agrego modelo de especialidades con metodos BREAD

---
+++ src/models/especialidades.model.js
@@ -0,0 +1,34 @@
+const pool = require('../db.js');
+
+const getAll = async () => {
+    const [rows] = await pool.query('SELECT * FROM especialidades WHERE activo = 1');
+    return rows;
+};
+
+const getById = async (id) => {
+    const [rows] = await pool.query('SELECT * FROM especialidades WHERE id_especialidad = ? AND activo = 1', [id]);
+    return rows[0];
+};
+
+const create = async (data) => {
+    const [result] = await pool.query('INSERT INTO especialidades (nombre, activo) VALUES (?, 1)', [data.nombre]);
+    return result;
+};
+
+const update = async (id, data) => {
+    const [result] = await pool.query('UPDATE especialidades SET nombre = ? WHERE id_especialidad = ? AND activo = 1', [data.nombre, id]);
+    return result;
+};
+
+const remove = async (id) => {
+    const [result] = await pool.query('UPDATE especialidades SET activo = 0 WHERE id_especialidad = ? AND activo = 1', [id]);
+    return result;
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
